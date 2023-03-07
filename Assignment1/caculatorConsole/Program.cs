d32149ee416da20880ccdbe45044cb770e10c077
curtaincall <curtaincall_99@163.com>
2023-03-08 07:07:17 +0800

    assignment1

---
+++ Assignment1/caculatorConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sys = true;
+            while (sys)
+            {
+                int operandA;
+                int.TryParse(Console.ReadLine(),out operandA);
+
+                char operatorM;
+                char.TryParse(Console.ReadLine(), out operatorM);
+
+                int operandB;
+                int.TryParse(Console.ReadLine(), out operandB);
+
+                switch (operatorM)
+                {
+                    case '+':
+                        Console.WriteLine("ANSWER");
+                        Console.WriteLine(operandA + operandB);
+                        Console.WriteLine("");
+                        break;
+                    case '-':
+                        Console.WriteLine("ANSWER");
+                        Console.WriteLine(operandA - operandB);
+                        Console.WriteLine("");
+                        break;
+                    case '*':
+                        Console.WriteLine("ANSWER");
+                        Console.WriteLine(operandA * operandB);
+                        Console.WriteLine("");
+                        break;
+                    case '/':
+                        Console.WriteLine("ANSWER");
+                        Console.WriteLine(operandA / operandB);
+                        Console.WriteLine("");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Expression");
+                        sys = false;
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
