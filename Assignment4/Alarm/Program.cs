5d74a17f36fa46852a387420478681b054b6c974
curtaincall <curtaincall_99@163.com>
2023-03-22 07:34:25 +0800

    第四次作业，由于比赛时间紧张，第二题尚未完善，本周内补完

--- Assignment4/Alarm/Program.cs
+++ Assignment4/Alarm/Program.cs
@@ -4,8 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Alarm
+namespace Alarms
 {
+    public 
+
+    public delegate void Tick(object sender, );
+    public delegate void Alarm();
+
+    public class Alarms
+    {
+        public event Tick OnTick;
+        public event Alarm OnAlarm;
+    }
+
+
     class Program
     {
         static void Main(string[] args)
